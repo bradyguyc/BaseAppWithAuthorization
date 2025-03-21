035ceab8c8af6077d63f8936e773b689b1eb6e32
Brady Guy Chambers <81129882+bradyguyc@users.noreply.github.com>
2025-03-21 15:19:12 -0500

    Enhance authentication and UI in MainView
    Updated MainViewViewModel.cs with new using directives, improved token handling in SignIn, and added CallProtectedFunction command. Reformatted MainView.xaml for better layout and added ListView for IdTokenClaims. Introduced LoginBranding CSS.css for enhanced styling and visual consistency.

--- BaseAppWithAuthorization/ViewModels/MainViewViewModel.cs
+++ BaseAppWithAuthorization/ViewModels/MainViewViewModel.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http.Headers;
 using System.Runtime.ConstrainedExecution;
 using System.Text;
+
 using BaseAppWithAuthorization.MSALClient;
+
 using CommunityToolkit.Mvvm;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+
 using Microsoft.Identity.Client;
 
 
@@ -14,7 +18,7 @@ namespace BaseAppWithAuthorization.ViewModels
     public partial class MainViewViewModel : ObservableObject
     {
         [ObservableProperty] bool isSignedIn = false;
-        [ObservableProperty] List<string> idTokenClaims = new ();
+        [ObservableProperty] List<string> idTokenClaims = new();
         public MainViewViewModel()
         {
             IsSignedIn = false;
@@ -23,31 +27,49 @@ namespace BaseAppWithAuthorization.ViewModels
             {
                 IsSignedIn = true;
                 UpdateClaims();
-                
+
             }
         }
         async Task UpdateClaims()
         {
             _ = await PublicClientSingleton.Instance.AcquireTokenSilentAsync();
-            
+
             var claims = PublicClientSingleton.Instance.MSALClientHelper.AuthResult.ClaimsPrincipal.Claims.Select(c => c.Value);
             IdTokenClaims = claims.ToList();
         }
         [RelayCommand]
         public Task SignIn()
         {
-            IAccount cachedUserAccount = PublicClientSingleton.Instance.MSALClientHelper.FetchSignedInUserFromCache().Result;
-
+            IAccount? cachedUserAccount = null; ;
+                cachedUserAccount  = PublicClientSingleton.Instance.MSALClientHelper.FetchSignedInUserFromCache().Result;
+         
+            
             return MainThread.InvokeOnMainThreadAsync(async () =>
             {
                 if (cachedUserAccount == null)
                 {
-                    await PublicClientSingleton.Instance.AcquireTokenSilentAsync();
-                    IAccount cachedUserAccount = PublicClientSingleton.Instance.MSALClientHelper.FetchSignedInUserFromCache().Result;
-                    if (cachedUserAccount != null)
+                    string token = null;
+                    try
                     {
-                        IsSignedIn = true;
-                        await UpdateClaims();
+                        token = await PublicClientSingleton.Instance.AcquireTokenSilentAsync();
+                    } catch (Exception ex)
+                    {
+                       
+                        Console.WriteLine(ex.Message);
+                    }
+                    if (token == null)
+                    {
+                        //todo: display error message to user that they need to sign in and the sign process was exited before completing sign in   
+                    }
+                    else
+                    {
+
+                        IAccount cachedUserAccount = PublicClientSingleton.Instance.MSALClientHelper.FetchSignedInUserFromCache().Result;
+                        if (cachedUserAccount != null)
+                        {
+                            IsSignedIn = true;
+                            await UpdateClaims();
+                        }
                     }
                 }
             });
@@ -61,5 +83,31 @@ namespace BaseAppWithAuthorization.ViewModels
                 IsSignedIn = false;
             });
         }
+        [RelayCommand]
+        public async Task CallProtectedFunction()
+        {
+            // string token = PublicClientSingleton.Instance.MSALClientHelper.AuthResult.AccessToken;
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    client.Timeout = TimeSpan.FromSeconds(130); // Set custom timeout value
+                    //string[] scopes = { "https://MyNextbook.onmicrosoft.com/MyNextBookAPIs/GetSecrets.Read" };
+                    string token  = await PublicClientSingleton.Instance.AcquireTokenSilentAsync();
+                    //todo: check if authResult is null and expiration
+                    //tring token = authResult.AccessToken;
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+
+                    string functionUrl = "https://myrecipebookmakerbackend.azurewebsites.net/api/GetSecrets";
+                    HttpResponseMessage response = await client.GetAsync(functionUrl);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
     }
+
 }
